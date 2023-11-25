d07c99ad2c41f4bd71aea68fd93b55c26f48a352
gursu <gheorgheursu1991@gmail.com>
2023-11-25 11:21:54 +0200

    Initial commit

---
+++ src/main/java/gursu/orange/technicaltask/OrangeTechnicalTaskApplication.java
@@ -0,0 +1,13 @@
+package gursu.orange.technicaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTechnicalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeTechnicalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gursu/orange/technicaltask/OrangeTechnicalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package gursu.orange.technicaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeTechnicalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
